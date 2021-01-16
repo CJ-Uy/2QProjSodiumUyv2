0e537337c4719a33f34ee33032dfb6794c400677
Charles Joshua Uy <charlesjoshuauy@gmail.com>
2021-01-17 00:20:19 +0800

    added delay

--- mmfiles/javascript/loading.js
+++ mmfiles/javascript/loading.js
@@ -1,6 +1,10 @@
 function hide_loader() {
   const loader = document.querySelector('.loader');
-  loader.className += ' hidden';
+
+  setTimeout(function() {
+    loader.className += ' hidden';
+  }, 1000)
+  
 }
 
 window.addEventListener('load', hide_loader());
